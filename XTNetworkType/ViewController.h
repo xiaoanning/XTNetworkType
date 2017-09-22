5d9b8786f030773f80e97345ddbcd2a3849fb261
xiaoanning <734871221@qq.com>
2017-09-22 11:12:46 +0800

    Initial Commit

---
+++ XTNetworkType/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XTNetworkType
+//
+//  Created by 安宁 on 2017/9/22.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
